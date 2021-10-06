d025682ecbf3d2323be5cecbecc4f324eba78ae3
Ismael Amezcua <ismaelamezcua@ucol.mx>
2021-10-05 20:17:02 -0500

    Small change to test Webhooks

--- a1-basic-building/src/main/java/mx/ucol/app/App.java
+++ a1-basic-building/src/main/java/mx/ucol/app/App.java
@@ -12,10 +12,20 @@ public class App
         int firstNumber = 10;
         int secondNumber = 5;
 
+        String environmentalVariable;
+
+        try {
+            environmentalVariable = System.getProperty("WSNSHELL_HOME");
+            System.out.println("EnvVar: " + environmentalVariable);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
         System.out.println( "This is the Basic Building app." );
         System.out.println("We can perform basic operations here: ");
         System.out.format("Addition: %d + %d = %d\n", firstNumber, secondNumber, op.addition(firstNumber, secondNumber));
         System.out.format("Substraction: %d + %d = %d\n", firstNumber, secondNumber, op.substraction(firstNumber, secondNumber));
         System.out.format("Multiplication: %d + %d = %d\n", firstNumber, secondNumber, op.multiplication(firstNumber, secondNumber));
+        // Made a change
     }
 }
